8507a029c954d3b47019186346236aaffcb4ff42
weifangni <1102034082@qq.com>
2022-12-08 20:22:31 +0800

    22
    22

--- pages/FacultyTeacher/BasicInformation/BasicInformation.js
+++ pages/FacultyTeacher/BasicInformation/BasicInformation.js
@@ -7,6 +7,7 @@ Page({
   onLoad(options) {
     console.log("20:15的测试内容")
     console.log("20:17的测试内容")
+    console.log("20:22的测试内容")
     this.setData({
       stuMessage:JSON.parse(options.stuMessage) ,
     })
