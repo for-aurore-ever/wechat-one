aba5a1d120f76f4b9aaad58a973a004a362ddda6
weifangni <1102034082@qq.com>
2022-12-08 20:25:27 +0800

    测试完毕
    内容清空

--- pages/FacultyTeacher/BasicInformation/BasicInformation.js
+++ pages/FacultyTeacher/BasicInformation/BasicInformation.js
@@ -5,9 +5,6 @@ Page({
     grade:''
   },
   onLoad(options) {
-    console.log("20:15的测试内容")
-    console.log("20:17的测试内容")
-    console.log("20:22的测试内容")
     this.setData({
       stuMessage:JSON.parse(options.stuMessage) ,
     })
