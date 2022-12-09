3787d17e4efcadbc009f98831488261809db8be9
weifangni <1102034082@qq.com>
2022-12-09 09:08:40 +0800

    11
    11

--- pages/FacultyTeacher/BasicInformation/BasicInformation.js
+++ pages/FacultyTeacher/BasicInformation/BasicInformation.js
@@ -5,6 +5,11 @@ Page({
     grade:''
   },
   onLoad(options) {
+<<<<<<< Updated upstream
+=======
+    console.log("20:15的测试内容")
+    console.log("20:18的测试内容")
+>>>>>>> Stashed changes
     this.setData({
       stuMessage:JSON.parse(options.stuMessage) ,
     })
