3b0280fb8a0390e6ad80884a97ee795a3b51e288
weifangni <1102034082@qq.com>
2022-12-09 09:10:47 +0800

    12.9
    测试

--- pages/FacultyTeacher/BasicInformation/BasicInformation.js
+++ pages/FacultyTeacher/BasicInformation/BasicInformation.js
@@ -5,11 +5,7 @@ Page({
     grade:''
   },
   onLoad(options) {
-<<<<<<< Updated upstream
-=======
-    console.log("20:15的测试内容")
-    console.log("20:18的测试内容")
->>>>>>> Stashed changes
+    console.log('测试9.10')
     this.setData({
       stuMessage:JSON.parse(options.stuMessage) ,
     })
